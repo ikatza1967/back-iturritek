7c37b82829c076e9e0cbb384d39eba0898d93d8b
ikatza1967 <ikatzacarbon1@gmail.com>
2023-10-16 12:21:54 +0200

    intento conexion

--- dbConfig.py
+++ dbConfig.py
@@ -1,3 +1,12 @@
+import sqlite3
+
 DATABASE_URI = 'bbdd.sqlite'
+
+try:
+    db=sqlite3.connect("bbdd.sqlite")
+    print("conexion exitosa")
+
+except sqlite3.Error:
+    print("error al conectar a la base de datos")
 
 
--- src/routes.py
+++ src/routes.py
@@ -1,3 +1,5 @@
+from dbConfig import *
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,7 +8,14 @@ app = Flask(__name__)
 def bienvenidos():
     return "hola"
 
-
+@app.route("/prueba")
+def get_usuarios():
+    cursor=db.cursor()
+    cursor.execute("SELECT * FROM usuarios")
+    usuarios=cursor.fetchall()
+    cursor.close()
+    return jsonify(usuarios)
 
 if __name__=="__main__":
     app.run (debug=True)
+    
