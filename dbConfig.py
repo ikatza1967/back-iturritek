99e58dc0f504ae265ac4b72a2687682907d344ec
Trontzal <trontzal@gmail.com>
2023-10-16 16:17:52 +0200

    bbdd se crea sola si no existe

--- dbConfig.py
+++ dbConfig.py
@@ -8,6 +8,22 @@ def get_db():
     if db is None:
         try:
             db = g._database = sqlite3.connect(DATABASE_URI)
+            cursor = db.cursor()
+
+            # Crea la tabla si no existe
+            cursor.execute('''
+                CREATE TABLE IF NOT EXISTS usuarios (
+                    id INTEGER PRIMARY KEY,
+                    nombre TEXT,
+                    apellidos TEXT,
+                    telefono TEXT,
+                    email TEXT,
+                    servicio TEXT,
+                    mensaje TEXT
+                )
+            ''')
+            db.commit()
+            
             print("Conexión exitosa a la base de datos")
         except sqlite3.Error as e:
             print(f"Error al conectar a la base de datos: {e}")
